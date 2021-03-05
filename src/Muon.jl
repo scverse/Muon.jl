346167d3694a0f5ba7254c88b8675478dfe78f32
Danila Bredikhin <danila.bredikhin@embl.de>
2021-03-05 20:28:22 +0100

    Create Muon.jl

---
+++ src/Muon.jl
@@ -0,0 +1,45 @@
+module Muon
+
+import HDF5
+
+struct AnnData
+    X::Array{Float64,2}
+end
+
+Base.@kwdef mutable struct MuData
+  mod::Union{Dict{String, AnnData}, Missing} = missing
+  file::HDF5.File
+
+  n_obs::Int64 = 0
+  n_var::Int64 = 0
+  
+  function MuData(Missing, file::HDF5.File, n_obs::Int64, n_var::Int64)
+    mdata = new(missing, file)
+    
+    mdata.n_obs = size(mdata.file["obs"]["_index"])[1]
+    mdata.n_var = size(mdata.file["var"]["_index"])[1]
+    mdata
+  end
+
+end
+
+function read(filename::AbstractString; backed=true)
+  if backed
+    fid = HDF5.h5open(filename)
+  else
+    fid = HDF5.h5open(filename, "r+")
+  end
+  mdata = MuData(file = fid)
+  return mdata 
+end
+
+Base.size(mdata::MuData) = (mdata.n_obs, mdata.n_var)
+
+Base.show(mdata::MuData) = print("""MuData object $(mdata.n_obs) \u2715 $(mdata.n_var)""")
+
+h5muopen = read
+
+export h5muopen, size
+export AnnData, MuData
+
+end # module
