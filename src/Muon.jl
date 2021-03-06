0e5bc10620aca3ee3cd94edc4947cc83dc13b338
Danila Bredikhin <danila.bredikhin@embl.de>
2021-03-06 21:22:28 +0100

    Add MuData.obsm

--- src/Muon.jl
+++ src/Muon.jl
@@ -10,11 +10,15 @@ Base.@kwdef mutable struct MuData
   mod::Union{Dict{String, AnnData}, Missing} = missing
   file::HDF5.File
 
+  obsm::Union{Dict{String, Any}, Missing} = missing
+
   n_obs::Int64 = 0
   n_var::Int64 = 0
   
-  function MuData(Missing, file::HDF5.File, n_obs::Int64, n_var::Int64)
+  function MuData(Missing, file::HDF5.File, obsm, n_obs::Int64, n_var::Int64)
     mdata = new(missing, file)
+
+    mdata.obsm = HDF5.read(file["obsm"])
     
     mdata.n_obs = size(mdata.file["obs"]["_index"])[1]
     mdata.n_var = size(mdata.file["var"]["_index"])[1]
