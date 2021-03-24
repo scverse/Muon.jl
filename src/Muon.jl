05e844d679bf9cc71a6262f889d4523e6c2d4dbf
Danila Bredikhin <danila.bredikhin@embl.de>
2021-03-24 19:59:44 +0100

    Fix include order

--- src/Muon.jl
+++ src/Muon.jl
@@ -11,8 +11,8 @@ export readh5mu, readh5ad, writeh5mu, writeh5ad, isbacked
 export AnnData, MuData
 
 include("util.jl")
-include("hdf5_io.jl")
 include("sparsedataset.jl")
+include("hdf5_io.jl")
 include("anndata.jl")
 include("mudata.jl")
 
