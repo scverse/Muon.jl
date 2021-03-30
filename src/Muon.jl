72ad4dcf2ea48046ab2dbfb80e4db79532a31852
Ilia Kats <ilia-kats@gmx.net>
2021-03-30 16:42:24 +0200

    fix include order

--- src/Muon.jl
+++ src/Muon.jl
@@ -10,12 +10,12 @@ using CategoricalArrays
 export readh5mu, readh5ad, writeh5mu, writeh5ad, isbacked
 export AnnData, MuData
 
-include("util.jl")
 include("sparsedataset.jl")
 include("transposeddataset.jl")
 include("hdf5_io.jl")
 include("alignedmapping.jl")
 include("anndata.jl")
 include("mudata.jl")
+include("util.jl")
 
 end # module
