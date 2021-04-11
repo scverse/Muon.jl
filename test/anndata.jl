7f24ab8739f497c5e0c2d737dc5a42c2007859eb
Danila Bredikhin <danila.bredikhin@embl.de>
2021-04-11 03:46:59 +0200

    Add tests for AnnData and MuData slicing

--- test/anndata.jl
+++ test/anndata.jl
@@ -1,6 +1,20 @@
-@testset "create adata" begin
-  n = 100
-  x = rand(Float64, (n, 10))
-  ad = AnnData(X=x)
-  @test size(ad)[1] == n
+n = 100
+d = 10
+x = rand(Float64, (n, d))
+ad = AnnData(X=x)
+
+@testset "create anndata" begin
+  @test size(ad, 1) == n
+end
+
+
+@testset "slicing anndata" begin
+  for i = 2:n
+    @test size(ad[collect(1:i),:]) == (i, d)
+  end
+  for i = 1:(n-1)
+    @test size(ad[collect(i:i+1),:]) == (2, d)
+  end
+  @test size(ad[["1", "2", "3"],:]) == (3, d)
+  @test size(ad[fill(true, n),:]) == (n, d)
 end
--- test/mudata.jl
+++ test/mudata.jl
@@ -1,15 +1,31 @@
+n = 100
+d1 = 10
+d2 = 20
+
+x = rand(Float64, (n, d1))
+ad = AnnData(X=x)
+
+y = rand(Float64, (n, d2))
+ad2 = AnnData(X=y)
+
+md = MuData(mod=Dict("ad1" => ad, "ad2" => ad2))
+
 @testset "create mudata" begin
-  n = 100
-  d1 = 10
-  d2 = 20
-  x = rand(Float64, (n, d1))
-  ad = AnnData(X=x)
-  y = rand(Float64, (n, d2))
-  ad2 = AnnData(X=y)
-  md = MuData(mod=Dict("ad1" => ad, "ad2" => ad2))
-  @test size(md)[1] == n
-  @test size(md)[2] == max(d1, d2)
+  @test size(md, 1) == n
+  @test size(md, 2) == max(d1, d2)
   
   modalities = collect(keys(md.mod))
   @test modalities == ["ad1", "ad2"]
+end
+
+@testset "slicing mudata" begin
+  d = max(d1, d2)
+  for i = 2:n
+    @test size(md[collect(1:i),:]) == (i, d)
+  end
+  for i = 1:(n-1)
+    @test size(md[collect(i:i+1),:]) == (2, d)
+  end
+  @test size(md[["1", "2", "3"],:]) == (3, d)
+  @test size(md[fill(true, n),:]) == (n, d)
 end
