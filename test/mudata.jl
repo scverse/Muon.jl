68bb9e1a6c37b88122f43bfc9d01bfc5921195e3
Ilia Kats <ilia-kats@gmx.net>
2021-04-13 17:11:15 +0200

    fix tests

--- test/mudata.jl
+++ test/mudata.jl
@@ -12,19 +12,19 @@ md = MuData(mod=Dict("ad1" => ad, "ad2" => ad2))
 
 @testset "create mudata" begin
   @test size(md, 1) == n
-  @test size(md, 2) == max(d1, d2)
-  
-  modalities = collect(keys(md.mod))
+  @test size(md, 2) == d1 + d2
+
+  modalities = sort(collect(keys(md.mod)))
   @test modalities == ["ad1", "ad2"]
 end
 
 @testset "slicing mudata" begin
-  d = max(d1, d2)
+  d = d1 + d2
   for i = 2:n
-    @test size(md[collect(1:i),:]) == (i, d)
+    @test size(md[1:i,:]) == (i, d)
   end
   for i = 1:(n-1)
-    @test size(md[collect(i:i+1),:]) == (2, d)
+    @test size(md[i:i+1,:]) == (2, d)
   end
   @test size(md[["1", "2", "3"],:]) == (3, d)
   @test size(md[fill(true, n),:]) == (n, d)
