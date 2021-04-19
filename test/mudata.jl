815638d6426d2a854cbad1d40ec07dafdce9d113
Ilia Kats <ilia-kats@gmx.net>
2021-04-19 16:14:44 +0200

    include expected warnings in MuData tests

--- test/mudata.jl
+++ test/mudata.jl
@@ -2,13 +2,15 @@ n = 100
 d1 = 10
 d2 = 20
 
+warn_msg = "Cannot join columns with the same name because var_names are intersecting."
+
 x = rand(Float64, (n, d1))
 ad = AnnData(X=x)
 
 y = rand(Float64, (n, d2))
 ad2 = AnnData(X=y)
 
-md = MuData(mod=Dict("ad1" => ad, "ad2" => ad2))
+@test_logs (:warn, warn_msg) md = MuData(mod=Dict("ad1" => ad, "ad2" => ad2))
 
 @testset "create mudata" begin
   @test size(md, 1) == n
@@ -21,11 +23,11 @@ end
 @testset "slicing mudata" begin
   d = d1 + d2
   for i = 2:n
-    @test size(md[1:i,:]) == (i, d)
+    @test (@test_logs (:warn, warn_msg) size(md[1:i,:])) == (i, d)
   end
   for i = 1:(n-1)
-    @test size(md[i:i+1,:]) == (2, d)
+    @test (@test_logs (:warn, warn_msg) size(md[i:i+1,:])) == (2, d)
   end
-  @test size(md[["1", "2", "3"],:]) == (3, d)
-  @test size(md[fill(true, n),:]) == (n, d)
+  @test (@test_logs (:warn, warn_msg) size(md[["1", "2", "3"],:])) == (3, d)
+  @test (@test_logs (:warn, warn_msg) size(md[fill(true, n),:])) == (n, d)
 end
